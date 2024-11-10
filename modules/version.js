721e7a3902320f8629171689fd96a1c512f06ac5
tyuXX <tyuxx@gsdistance.org>
2024-11-10 12:09:19 +0300

    hotfix2

--- modules/version.js
+++ modules/version.js
@@ -11,7 +11,7 @@ async function loadVersionInfo() {
     if (!response2.ok) {
       throw new Error(`Failed to fetch version info: ${response2.statusText}`);
     }
-    const versionData2 = response2.text;
+    const versionData2 = response2.text();
 
     // Display version information on the game UI
     const versionInfoContainer = document.getElementById('versionInfo');
